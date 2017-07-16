c83226354bff102168c6723ad6bcf35ebacabc63
Sylvain Hallé <shalle@acm.org>
2017-07-15 18:39:29 -0700

    Two minor fixes to the template

--- FantasticWindmill/templates/sidebar.php
+++ FantasticWindmill/templates/sidebar.php
@@ -6,7 +6,7 @@
 <div class="sidebar" id="sidebar">
   <div class="sidebar-item">
   <img src="/images/labpal-175.png" alt="LabPal"/>
-  <p>LabPal is a library for running experiments on a computer and process their results.</p>
+  <p>LabPal is a library for running experiments on a computer and processing their results.</p>
   </div>
 
   <nav class="sidebar-nav">
@@ -16,7 +16,7 @@
     <a class="sidebar-nav-item" href="/manual/index.html">User manual</a>
     <a class="sidebar-nav-item" href="https://github.com/liflab/labpal/releases/latest">Download</a>
     <a class="sidebar-nav-item" href="https://github.com/liflab/labpal">GitHub project</a>
-    <span class="sidebar-nav-item">Currently v2.7</span>
+    <span class="sidebar-nav-item">Currently v2.9</span>
   </nav>
 
   <div class="sidebar-item">
